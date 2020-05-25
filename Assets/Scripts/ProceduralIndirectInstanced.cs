5ea52264a17deb80aeb693462336522a432073bd
Chris Redmann <chris@cpredmann.com>
2020-05-25 04:53:36 -0700

    Basic Draw Procedural

---
+++ Assets/Scripts/ProceduralIndirectInstanced.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ProceduralIndirectInstanced : MonoBehaviour
+{
+
+  public ComputeShader computeShader;
+  public Material proceduralMaterial;
+  private ComputeBuffer indirectArgs;
+  private ComputeBuffer positionBuffer;
+  private ComputeBuffer colorBuffer;
+  private ComputeBuffer normalBuffer;
+  private ComputeBuffer indexBuffer;
+  private int generateKernel;
+
+  private Bounds bounds;
+
+  Vector4[] positionData;
+  Vector4[] colorData;
+  Vector4[] normalData;
+  int[] indexData;
+
+  const int triangles = 12;
+
+  void Start()
+  {
+    indirectArgs = new ComputeBuffer(4, sizeof(int));
+    positionBuffer = new ComputeBuffer(triangles * 3, 4 * sizeof(float));
+    colorBuffer = new ComputeBuffer(triangles * 3, 4 * sizeof(float));
+    normalBuffer = new ComputeBuffer(triangles * 3, 4 * sizeof(float));
+    indexBuffer = new ComputeBuffer(triangles * 3, sizeof(int));
+    generateKernel = computeShader.FindKernel("GenerateOutput");
+    Shader.SetGlobalBuffer("PositionBuffer", positionBuffer);
+    Shader.SetGlobalBuffer("ColorBuffer", colorBuffer);
+    Shader.SetGlobalBuffer("NormalBuffer", normalBuffer);
+    Shader.SetGlobalBuffer("IndexBuffer", indexBuffer);
+    positionData = new Vector4[positionBuffer.count];
+    colorData = new Vector4[colorBuffer.count];
+    normalData = new Vector4[normalBuffer.count];
+    indexData = new int[indexBuffer.count];
+    bounds = new Bounds(new Vector3(0.0f, 0.0f, 0.0f), new Vector3(20.0f, 20.0f, 20.0f));
+  }
+
+  void Update()
+  {
+    computeShader.Dispatch(generateKernel, triangles, 1, 1);
+    //positionBuffer.GetData(positionData);
+    //colorBuffer.GetData(colorData);
+    //normalBuffer.GetData(normalData);
+    //indexBuffer.GetData(indexData);
+    Graphics.DrawProcedural(proceduralMaterial, bounds, MeshTopology.Triangles, triangles * 3, 1, null, null, UnityEngine.Rendering.ShadowCastingMode.On, true);
+  }
+
+  private void OnDestroy()
+  {
+    indirectArgs.Dispose();
+    positionBuffer.Dispose();
+    colorBuffer.Dispose();
+    normalBuffer.Dispose();
+    indexBuffer.Dispose();
+  }
+}
---
+++ Assets/Shaders/ProceduralVertex.hlsl
@@ -0,0 +1,49 @@
+﻿//float4x4 _LocalToWorld;
+
+uniform StructuredBuffer<float4> PositionBuffer;
+uniform StructuredBuffer<float4> ColorBuffer;
+uniform StructuredBuffer<float4> NormalBuffer;
+
+uniform StructuredBuffer<int> IndexBuffer;
+
+struct Attributes
+{
+	uint vertexID : SV_VertexID;
+	UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+PackedVaryingsType ProceduralVertex(Attributes input)
+{
+	float4 vertex = PositionBuffer[input.vertexID];
+	float3 position = vertex.xyz;
+	float3 normal = NormalBuffer[input.vertexID].xyz;
+
+	AttributesMesh am;
+	am.positionOS = position;
+#ifdef ATTRIBUTES_NEED_NORMAL
+    am.normalOS = normal;
+#endif
+#ifdef ATTRIBUTES_NEED_TANGENT
+    am.tangentOS = 0;
+#endif
+#ifdef ATTRIBUTES_NEED_TEXCOORD0
+	am.uv0 = 0;
+#endif
+#ifdef ATTRIBUTES_NEED_TEXCOORD1
+    am.uv1 = 0;
+#endif
+#ifdef ATTRIBUTES_NEED_TEXCOORD2
+    am.uv2 = 0;
+#endif
+#ifdef ATTRIBUTES_NEED_TEXCOORD3
+    am.uv3 = 0;
+#endif
+#ifdef ATTRIBUTES_NEED_COLOR
+	am.color = ColorBuffer[input.vertexID];
+#endif
+    UNITY_TRANSFER_INSTANCE_ID(input, am);
+
+    VaryingsType varyingsType;
+    varyingsType.vmesh = VertMesh(am);
+    return PackVaryingsType(varyingsType);
+}
