e836358df430a1ba39d006e4e13bfc49fbd9d0cd
Chris Redmann <credmann@unity3d.com>
2020-08-03 05:45:45 -0700

    Renaming and cleanup

--- Assets/Scripts/ProceduralIndirectInstanced.cs
+++ Assets/Scripts/ProceduralIndirectInstanced.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -36,6 +35,8 @@ public class ProceduralIndirectInstanced : MonoBehaviour
     private List<Vector3> inputNormals;
     private List<Vector4> inputColors;
 
+    private CommandBuffer commandBuffer;
+    
     void Start()
     {
         inputPositions = new List<Vector3>();
@@ -83,24 +84,40 @@ public class ProceduralIndirectInstanced : MonoBehaviour
 
         bounds = new Bounds(new Vector3(0.0f, 0.0f, 0.0f), new Vector3(200.0f, 200.0f, 200.0f));
         computeShader.Dispatch(shapeKernel, loops, sides, 1);
+        
+        BuildCommandBuffer();
+    }
+
+    void BuildCommandBuffer()
+    {
+        commandBuffer = new CommandBuffer {name = "DrawProc"};
+        commandBuffer.BeginSample("Compute");
+        commandBuffer.DispatchCompute(computeShader, xformKernel, count, 1, 1);
+        commandBuffer.DispatchCompute(computeShader, colorKernel, count, 1, 1);
+        commandBuffer.EndSample("Compute");
+        //commandBuffer.BeginSample("Draw");
+        //commandBuffer.DrawProceduralIndirect(Matrix4x4.identity, proceduralMaterial, -1, MeshTopology.Triangles, indirectArgs, 0);
+        //commandBuffer.EndSample("Draw");
+        
     }
 
     void Update()
     {
-        computeShader.Dispatch(xformKernel, count, 1, 1);
-        computeShader.Dispatch(colorKernel, count, 1, 1);
+        Graphics.ExecuteCommandBuffer(commandBuffer);
+        //computeShader.Dispatch(xformKernel, count, 1, 1);
+        //computeShader.Dispatch(colorKernel, count, 1, 1);
         Graphics.DrawProceduralIndirect(proceduralMaterial, bounds, MeshTopology.Triangles, indirectArgs, 0, null, null, UnityEngine.Rendering.ShadowCastingMode.On, true);
     }
 
     private void OnDestroy()
     {
-        indirectArgs.Dispose();
-        positionBuffer.Dispose();
-        colorBuffer.Dispose();
-        normalBuffer.Dispose();
-        xformBuffer.Dispose();
-        shapePositionBuffer.Dispose();
-        shapeNormalBuffer.Dispose();
-        shapeIndexBuffer.Dispose();
+        indirectArgs?.Dispose();
+        positionBuffer?.Dispose();
+        colorBuffer?.Dispose();
+        normalBuffer?.Dispose();
+        xformBuffer?.Dispose();
+        shapePositionBuffer?.Dispose();
+        shapeNormalBuffer?.Dispose();
+        shapeIndexBuffer?.Dispose();
     }
 }
---
+++ Assets/Shaders/Instanced/InstancedVertex.hlsl
@@ -0,0 +1,49 @@
+﻿uniform StructuredBuffer<float4> ColorBuffer;
+
+uniform StructuredBuffer<int> ShapeIndexBuffer;
+uniform StructuredBuffer<float3> ShapePointBuffer;
+uniform StructuredBuffer<float3> ShapeNormalBuffer;
+
+uniform StructuredBuffer<int> IndexBuffer;
+uniform StructuredBuffer<matrix> XformBuffer;
+
+struct Attributes
+{
+	uint vertexID : SV_VertexID;
+	UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+PackedVaryingsType InstancedVertex(Attributes input, uint instanceID : SV_InstanceID)
+{
+	float4 vertex = mul(XformBuffer[instanceID], float4(ShapePointBuffer[ShapeIndexBuffer[input.vertexID]], 1.0f));
+	float3 normal = ShapeNormalBuffer[ShapeIndexBuffer[input.vertexID]];
+
+	AttributesMesh am;
+	am.positionOS = vertex.xyz;
+#ifdef ATTRIBUTES_NEED_NORMAL
+    am.normalOS = normal;
+#endif
+#ifdef ATTRIBUTES_NEED_TANGENT
+    am.tangentOS = 0;
+#endif
+#ifdef ATTRIBUTES_NEED_TEXCOORD0
+	am.uv0 = 0;
+#endif
+#ifdef ATTRIBUTES_NEED_TEXCOORD1
+    am.uv1 = 0;
+#endif
+#ifdef ATTRIBUTES_NEED_TEXCOORD2
+    am.uv2 = 0;
+#endif
+#ifdef ATTRIBUTES_NEED_TEXCOORD3
+    am.uv3 = 0;
+#endif
+#ifdef ATTRIBUTES_NEED_COLOR
+	am.color = ColorBuffer[instanceID];
+#endif
+    UNITY_TRANSFER_INSTANCE_ID(input, am);
+
+    VaryingsType varyingsType;
+    varyingsType.vmesh = VertMesh(am);
+    return PackVaryingsType(varyingsType);
+}
