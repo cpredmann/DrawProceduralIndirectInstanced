491cb87b9619441241c6184a183485630c6782f6
Chris Redmann <credmann@unity3d.com>
2020-08-03 06:21:05 -0700

    Basic draw procedural

---
+++ Assets/Scripts/ProceduralIndirect.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ProceduralIndirect : MonoBehaviour
+{
+
+    public Material material;
+    private Bounds bounds;
+    private ComputeBuffer indexBuffer;
+    private ComputeBuffer vertexBuffer;
+    private CommandBuffer commandBuffer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+         bounds = new Bounds(Vector3.zero, new Vector3(10.0f, 10.0f, 10.0f));
+         int[] indices = new[] {0, 1, 2};
+         Vector4[] vertices = new[]
+         {
+             new Vector4(0.0f, 0.0f, 0.0f, 0.0f), 
+             new Vector4(0.0f, 1.0f, 0.0f, 0.0f), 
+             new Vector4(0.0f, 0.0f, 1.0f, 0.0f), 
+         };
+         indexBuffer = new ComputeBuffer(3, sizeof(int));
+         vertexBuffer = new ComputeBuffer(3, 4 * sizeof(float));
+         indexBuffer.SetData(indices);
+         vertexBuffer.SetData(vertices);
+         material.SetBuffer("IndexBuffer", indexBuffer);
+         material.SetBuffer("VertexBuffer", vertexBuffer);
+         BuildCommandBuffer();
+    }
+
+    void BuildCommandBuffer()
+    {
+        commandBuffer = new CommandBuffer() {name = "DrawProc"};
+        commandBuffer.DrawProcedural(Matrix4x4.identity, material, -1, MeshTopology.Triangles, 3, 1, null);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Graphics.DrawProcedural(material, bounds, MeshTopology.Triangles, 3); 
+        //Graphics.ExecuteCommandBuffer(commandBuffer); 
+    }
+
+    private void OnDestroy()
+    {
+        indexBuffer?.Dispose();
+        vertexBuffer?.Dispose();
+    }
+}
--- Assets/Shaders/Procedural/ProceduralVertex.hlsl
+++ Assets/Shaders/Procedural/ProceduralVertex.hlsl
@@ -1,27 +1,19 @@
-﻿uniform StructuredBuffer<float4> ColorBuffer;
-
-uniform StructuredBuffer<int> ShapeIndexBuffer;
-uniform StructuredBuffer<float3> ShapePointBuffer;
-uniform StructuredBuffer<float3> ShapeNormalBuffer;
-
-uniform StructuredBuffer<int> IndexBuffer;
-uniform StructuredBuffer<matrix> XformBuffer;
+﻿uniform StructuredBuffer<int> IndexBuffer;
+uniform StructuredBuffer<float4> VertexBuffer;
 
 struct Attributes
 {
 	uint vertexID : SV_VertexID;
-	UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
-PackedVaryingsType ProceduralVertex(Attributes input, uint instanceID : SV_InstanceID)
+PackedVaryingsType ProceduralVertex(Attributes input)
 {
-	float4 vertex = mul(XformBuffer[instanceID], float4(ShapePointBuffer[ShapeIndexBuffer[input.vertexID]], 1.0f));
-	float3 normal = ShapeNormalBuffer[ShapeIndexBuffer[input.vertexID]];
+    float4 vertex = VertexBuffer[input.vertexID];	
 
 	AttributesMesh am;
 	am.positionOS = vertex.xyz;
 #ifdef ATTRIBUTES_NEED_NORMAL
-    am.normalOS = normal;
+    am.normalOS = 0;
 #endif
 #ifdef ATTRIBUTES_NEED_TANGENT
     am.tangentOS = 0;
@@ -39,9 +31,8 @@ PackedVaryingsType ProceduralVertex(Attributes input, uint instanceID : SV_Insta
     am.uv3 = 0;
 #endif
 #ifdef ATTRIBUTES_NEED_COLOR
-	am.color = ColorBuffer[instanceID];
+	am.color = 0;
 #endif
-    UNITY_TRANSFER_INSTANCE_ID(input, am);
 
     VaryingsType varyingsType;
     varyingsType.vmesh = VertMesh(am);
